52358766316caf93e3207b87959ffb045224d3d7
NadineTsianta <k.tsianta@rocketmail.com>
2024-03-05 01:26:05 +0000

    adding cards to project page

--- src/components/ProjectPage/ProjectsPage.jsx
+++ src/components/ProjectPage/ProjectsPage.jsx
@@ -1,10 +1,24 @@
-const HomeItem = ()=> {
-    return (
-        <div>
-            <h1>hello</h1>
-
+const ProjectEl = () => {
+  return (
+    <div>
+      <div class="card" style="width: 18rem;">
+        <img
+          src="../src/assets/Bootstrap-Portfolio.png"
+          class="card-img-top"
+          alt="Portfolio Image"
+        />
+        <div class="card-body">
+          <h5 class="card-title">Bootrap Portfolio</h5>
+          <p class="card-text">
+          This project's requirement was to leverage Bootstrap elements to craft a dynamic portfolio showcasing my latest work. It builds upon the previous portfolio, which was created exclusively using HTML
+          </p>
+          <a href="https://nadinetsianta.github.io/Bootstrap-Portfolio/" class="btn btn-primary">
+            Visit
+          </a>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default HomeItem
+export default ProjectEl;
